f0415c1827c8c893a3b3bca66fbb24834145911e
litalsh <lital.shalom@gmail.com>
2020-01-19 10:32:19 +0200

    CategoryMenu component updated

--- src/components/CategoryMenu/CategoryMenu.js
+++ src/components/CategoryMenu/CategoryMenu.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './CategoryMenu.scss';
 
+
 const CategoryMenu = () => {
+  let menuTitle = [
+    "סטריט פוד", 
+    "אטריות ביצים", 
+    "אטריות מיוחדות", 
+    "אטריות אורז", 
+    "אורז ועיקיות", 
+    "קומבינציות", 
+    "מרקים", 
+    "גו נודלס טבעוני", 
+    "גו נודלס ללא גלוטן", 
+    "קינוחים", 
+    "שתיה קלה", 
+    "בירות"];
+
   return (
     <nav className="CategoryMenu">
       <ul>
-        <li><a href="#">סטריט פוד</a></li>
-        <li><a href="#">אטריות ביצים</a></li>
-        <li><a href="#">אטריות מיוחדות</a></li>
-        <li><a href="#">אטריות אורז</a></li>
-        <li><a href="#">אורז ועיקיות</a></li>
-        <li><a href="#">קומבינציות</a></li>
-        <li><a href="#">מרקים</a></li>
-        <li><a href="#">גו נודלס טבעוני</a></li>
-        <li><a href="#">גו נודלס ללא גלוטן</a></li>
-        <li><a href="#">קינוחים</a></li>
-        <li><a href="#">שתיה קלה</a></li>
-        <li><a href="#">בירות</a></li>
+        {menuTitle.map(title => {return(<li><a href="#">{title}</a></li>)})}
       </ul>
     </nav>
   )

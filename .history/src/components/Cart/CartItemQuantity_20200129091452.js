7b268e9bb1191b2cdf34e4c9315a12a48acd1d1f
litalsh <lital.shalom@gmail.com>
2020-01-29 23:24:19 +0200

    updated

---
+++ .history/src/components/Cart/CartItemQuantity_20200129091452.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import './CartItem.scss';
+
+import DishPopup from '../DishPopup/DishPopup.js';
+
+const CartItemQuantity = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  return (
+    <div className="cartItemQuantity">
+      <button onClick={<DishPopup />}>🖊️</button>
+      <button onClick={() => setQuantity(quantity + 1)}>+</button>
+      <span>{quantity}</span>
+      <button onClick={() => setQuantity(quantity - 1)}>-</button>
+    </div>
+  )
+}
+
+export default CartItemQuantity;

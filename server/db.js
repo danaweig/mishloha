6e1b7b507f1be8bd3a4abaf98b1780906b2944a5
danaweig <dana.weigler@gmail.com>
2020-02-29 21:03:26 +0200

    images added

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/mishloha';
+const dbUrl = process.env.DB_URL || 'mongodb+srv://mishloha_mda:mishloha1234@dwcluster-l4ts5.mongodb.net/test?retryWrites=true&w=majority';
 
 async function setDb() {
     await mongoose.connect(dbUrl, {
